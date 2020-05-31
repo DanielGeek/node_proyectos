97c85dcc843d63c30445ef6322341ef6420afa4e
DanielGeek <daniel30081990@gmail.com>
2020-05-31 00:23:44 -0400

    Usando parciales con HBS

--- node_ejemplos/06_webserver/server.js
+++ node_ejemplos/06_webserver/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 
+const hbs = require('hbs');
+
 app.use(express.static(__dirname + '/public'));
  
 // express hbs engine
+hbs.registerPartials(__dirname + '/views/parciales' );
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     
     res.render('home', {
         nombre: 'Daniel',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    
+    res.render('about', {
         anio: new Date().getFullYear()
     });
 });
