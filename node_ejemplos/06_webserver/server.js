673fd66054eba5686eb05d5275907a152334f098
DanielGeek <daniel30081990@gmail.com>
2020-05-30 23:56:40 -0400

    Handlebars

--- node_ejemplos/06_webserver/server.js
+++ node_ejemplos/06_webserver/server.js
@@ -3,15 +3,16 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
  
-// app.get('/', (req, res) => {
-// //   res.send('Hola Mundo');
-//     let salida = {
-//         nombre: 'Daniel',
-//         edad: 29,
-//         url: req.url
-//     }
-//     res.send(salida);
-// });
+// express hbs engine
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    
+    res.render('home', {
+        nombre: 'Daniel',
+        anio: new Date().getFullYear()
+    });
+});
  
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
