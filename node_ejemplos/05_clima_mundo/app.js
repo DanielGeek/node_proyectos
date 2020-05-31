bc41e92ab01c1cfbee3c06cec5ca3b663c96262c
DanielGeek <daniel30081990@gmail.com>
2020-05-30 22:41:26 -0400

    Conectando ambos servicios

--- node_ejemplos/05_clima_mundo/app.js
+++ node_ejemplos/05_clima_mundo/app.js
@@ -16,6 +16,22 @@ const argv = require('yargs').options({
 // lugar.getLugarLatLng( argv.direccion )
 //         .then( console.log );
 
-clima.getClima( 40.750000, -74.000000 )
-        .then( console.log )
-        .catch(console.log);
+// clima.getClima( 40.750000, -74.000000 )
+//         .then( console.log )
+//         .catch(console.log);
+
+const getInfo = async( direccion ) => {
+
+
+    try {
+        const coords = await lugar.getLugarLatLng( argv.direccion );
+        const temp = await clima.getClima( coords.lat, coords.lng );
+        return `El clima de ${ coords.direccion } es de ${ temp }.`;
+    } catch (e) {
+        return `No se pudo determinar el clima de ${ direccion }`;        
+    }
+}
+
+getInfo( argv.direccion)
+    .then( console.log )
+    .catch( console.log );
