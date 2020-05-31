ef2c9759008ea7cfca0367ccc53f059a8ba9f52e
DanielGeek <daniel30081990@gmail.com>
2020-05-30 23:20:38 -0400

    Servir contenido estático

--- node_ejemplos/06_webserver/server.js
+++ node_ejemplos/06_webserver/server.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
  
-app.get('/', (req, res) => {
-//   res.send('Hola Mundo');
-    let salida = {
-        nombre: 'Daniel',
-        edad: 29,
-        url: req.url
-    }
-    res.send(salida);
-});
+// app.get('/', (req, res) => {
+// //   res.send('Hola Mundo');
+//     let salida = {
+//         nombre: 'Daniel',
+//         edad: 29,
+//         url: req.url
+//     }
+//     res.send(salida);
+// });
  
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
