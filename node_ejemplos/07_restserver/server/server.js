c60bd9310479b71d5d2a0926df2961643ba1aa37
DanielGeek <daniel30081990@gmail.com>
2020-05-31 21:55:15 -0400

    Peticiones HTTP - GET - PUT - POST - DELETE

--- node_ejemplos/07_restserver/server/server.js
+++ node_ejemplos/07_restserver/server/server.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
  
 app.get('/', function (req, res) {
-  res.json('Hello World')
-})
+    res.json('get Usuario');
+});
  
+app.post('/usuario', function (req, res) {
+
+    let body = req.body;
+
+    res.json({
+        persona: body
+    });
+});
+
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+ 
+app.delete('/delete', function (req, res) {
+    res.json('delete Usuario');
+});
+
 app.listen(3000, ( )=> {
     console.log('Escuchando en el puerto: ', 3000);
 });
