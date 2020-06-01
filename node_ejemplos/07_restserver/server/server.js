b2fa5b770c1272ff4d93dc6a6f0d9955bdee480f
DanielGeek <daniel30081990@gmail.com>
2020-05-31 21:29:06 -0400

    Iniciando el proyecto - RESTServe

---
+++ node_ejemplos/07_restserver/server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/', function (req, res) {
+  res.json('Hello World')
+})
+ 
+app.listen(3000, ( )=> {
+    console.log('Escuchando en el puerto: ', 3000);
+});
