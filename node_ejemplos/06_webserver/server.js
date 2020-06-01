854bef20ec5ef341f155b3be72d1fe8a70458a60
DanielGeek <daniel30081990@gmail.com>
2020-05-31 20:46:12 -0400

    variable de puerto

--- node_ejemplos/06_webserver/server.js
+++ node_ejemplos/06_webserver/server.js
@@ -4,6 +4,8 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
  
 // express hbs engine
@@ -22,6 +24,6 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
  
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
