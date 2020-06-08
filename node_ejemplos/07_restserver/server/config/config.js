3d7289cad49a23359745f675a016a70fefc8f8ce
DanielGeek <daniel30081990@gmail.com>
2020-06-07 23:58:25 -0400

    Generar un JWT

--- node_ejemplos/07_restserver/server/config/config.js
+++ node_ejemplos/07_restserver/server/config/config.js
@@ -1,3 +1,13 @@
 // Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+// vencimiento token
+// 60 segundo
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// SEED de autenticacion
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
--- node_ejemplos/07_restserver/server/routes/login.js
+++ node_ejemplos/07_restserver/server/routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const Usuario = require('../models/usuario');
 
@@ -8,10 +9,49 @@ const app = express();
 
 app.post('/login', (req, res) => {
 
-    res.json({
-        ok: true
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email}, (err, usuarioDB) => {
+        
+        if( err ) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if( !usuarioDB ) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o contraseña incorrectos'
+                }
+            });
+        }
+
+        //encripto la password y luego comparo con la encriptada en la bd
+        if( !bcrypt.compareSync( body.password, usuarioDB.password)){
+            
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o contraseña incorrectos'
+                }
+            });
+        }
+
+        let token = jwt.sign({
+            usuario: usuarioDB
+        },process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token
+        });
+
     });
-    
+
 });
 
 module.exports = app;
