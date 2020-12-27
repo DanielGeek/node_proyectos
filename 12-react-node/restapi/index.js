73181c25e730844bb4ac7d0cff7335f509756b87
DanielGeek <daniel30081990@gmail.com>
2020-12-27 08:13:21 -0300

    295. Creando el Modelo de Clientes

--- 12-react-node/restapi/index.js
+++ 12-react-node/restapi/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
-const mongoose = require('/mongoose');
+const mongoose = require('mongoose');
 
 // Conectar mongo
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/restapis', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 // Crear el servidor
