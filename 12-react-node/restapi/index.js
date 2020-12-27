33582840bdb9ca445dc0d85602c70a7891b73285
DanielGeek <daniel30081990@gmail.com>
2020-12-27 07:35:23 -0300

    293. Conectando nuestro Proyecto a MongoDB

--- 12-react-node/restapi/index.js
+++ 12-react-node/restapi/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('/mongoose');
+
+// Conectar mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapis', {
+    useNewUrlParser: true
+});
 
 // Crear el servidor
 const app = express();
