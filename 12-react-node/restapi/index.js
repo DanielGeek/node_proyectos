6dc1309814e6344e31b4f59a0845c40edb95791e
DanielGeek <daniel30081990@gmail.com>
2020-12-27 07:02:01 -0300

    290. Creando el Routing

--- 12-react-node/restapi/index.js
+++ 12-react-node/restapi/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const routes = require('./routes');
 
 // Crear el servidor
 const app = express();
+
+// Rutas de la app
+app.use('/', routes());
 
 // Puerto
 app.listen(5000);
