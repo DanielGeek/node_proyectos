a6f5682cb06488bad72d9b153c4e0622639508f2
DanielGeek <daniel30081990@gmail.com>
2020-12-29 23:36:47 -0300

    326. Agregando formulario para nuevos clientes

--- 12-react-node/cliente-api/src/componentes/clientes/NuevoCliente.js
+++ 12-react-node/cliente-api/src/componentes/clientes/NuevoCliente.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export const NuevoCliente = () => {
     return (
-        <h2>
-            Nuevo Cliente
-        </h2>
+        <Fragment>
+            <h2>Nuevo Cliente</h2>
+
+            <form>
+                <legend>Llena todos los campos</legend>
+
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input type="text" placeholder="Nombre Cliente" name="nombre" />
+                </div>
+
+                <div className="campo">
+                    <label>Apellido:</label>
+                    <input type="text" placeholder="Apellido Cliente" name="apellido" />
+                </div>
+
+                <div className="campo">
+                    <label>Empresa:</label>
+                    <input type="text" placeholder="Empresa Cliente" name="empresa" />
+                </div>
+
+                <div className="campo">
+                    <label>Email:</label>
+                    <input type="email" placeholder="Email Cliente" name="email" />
+                </div>
+
+                <div className="campo">
+                    <label>Teléfono:</label>
+                    <input type="email" placeholder="Teléfono Cliente" name="telefono" />
+                </div>
+
+                <div className="enviar">
+                    <input type="submit" className="btn btn-azul" value="Agregar Cliente" />
+                </div>
+
+            </form>
+        </Fragment>
     )
 }
