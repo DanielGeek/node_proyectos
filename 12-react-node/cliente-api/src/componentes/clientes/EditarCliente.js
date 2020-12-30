f238534ecc1afefb2a3b11a22f5fba30fabbe3f4
DanielGeek <daniel30081990@gmail.com>
2020-12-30 01:31:32 -0300

    332. Obteniendo el Cliente que se desea editar

--- 12-react-node/cliente-api/src/componentes/clientes/EditarCliente.js
+++ 12-react-node/cliente-api/src/componentes/clientes/EditarCliente.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import { withRouter } from 'react-router-dom';
 import clienteAxios from '../../config/axios';
@@ -9,7 +9,8 @@ const EditarCliente = (props) => {
     // obtener el ID
     const { id } = props.match.params;
 
-    const [cliente, guardarCliente] = useState({
+    // Cliente = state, datosCliente = función para guardar el state
+    const [cliente, datosCliente] = useState({
         nombre: '',
         apellido: '',
         empresa: '',
@@ -17,10 +18,26 @@ const EditarCliente = (props) => {
         telefono: ''
     });
 
+    // Destructuring
+    const { nombre, apellido, email, empresa, telefono } = cliente;
+
+    // Query a la API
+    const consultarAPI = async () => {
+        const clienteConsulta = await clienteAxios.get(`/clientes/${id}`);
+
+        // guardar en el state
+        datosCliente(clienteConsulta.data);
+    }
+
+    // useEffect, cuando el componente carga
+    useEffect(() => {
+        consultarAPI();
+    }, []);
+
     // Leer los datos del formulario
     const actualizarState = e => {
         // Almacenar lo que el usuario escribe en el state
-        guardarCliente({
+        datosCliente({
             // Obtener una copia del state actual
             ...cliente,
             [e.target.name]: e.target.value
@@ -29,8 +46,6 @@ const EditarCliente = (props) => {
 
     // Validar el formulario
     const validarCliente = () => {
-        // Destructuring
-        const { nombre, apellido, email, empresa, telefono } = cliente;
 
         // revisar que las propiedas del state tenga contenido
         let valido = !nombre.length || !apellido.length || !email.length || !empresa.length || !telefono.length;
@@ -41,38 +56,38 @@ const EditarCliente = (props) => {
 
     return (
         <Fragment>
-            <h2>Nuevo Cliente</h2>
+            <h2>Editar Cliente</h2>
 
             <form>
                 <legend>Llena todos los campos</legend>
 
                 <div className="campo">
                     <label>Nombre:</label>
-                    <input type="text" placeholder="Nombre Cliente" name="nombre" onChange={actualizarState} />
+                    <input type="text" placeholder="Nombre Cliente" name="nombre" onChange={actualizarState} value={nombre} />
                 </div>
 
                 <div className="campo">
                     <label>Apellido:</label>
-                    <input type="text" placeholder="Apellido Cliente" name="apellido" onChange={actualizarState} />
+                    <input type="text" placeholder="Apellido Cliente" name="apellido" onChange={actualizarState} value={apellido} />
                 </div>
 
                 <div className="campo">
                     <label>Empresa:</label>
-                    <input type="text" placeholder="Empresa Cliente" name="empresa" onChange={actualizarState} />
+                    <input type="text" placeholder="Empresa Cliente" name="empresa" onChange={actualizarState} value={empresa} />
                 </div>
 
                 <div className="campo">
                     <label>Email:</label>
-                    <input type="email" placeholder="Email Cliente" name="email" onChange={actualizarState} />
+                    <input type="email" placeholder="Email Cliente" name="email" onChange={actualizarState} value={email} />
                 </div>
 
                 <div className="campo">
                     <label>Teléfono:</label>
-                    <input type="tel" placeholder="Teléfono Cliente" name="telefono" onChange={actualizarState} />
+                    <input type="tel" placeholder="Teléfono Cliente" name="telefono" onChange={actualizarState} value={telefono} />
                 </div>
 
                 <div className="enviar">
-                    <input type="submit" className="btn btn-azul" value="Agregar Cliente" disabled={validarCliente()} />
+                    <input type="submit" className="btn btn-azul" value="Guardar cambios" disabled={validarCliente()} />
                 </div>
 
             </form>
