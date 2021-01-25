b18d276697d5d3e229f712bcee84f517163d896f
DanielGeek <daniel30081990@gmail.com>
2021-01-25 02:28:48 -0300

    fixed: fixed eslint

--- 13-tdd/class_01/src/controllers/register-vehicle.ts
+++ 13-tdd/class_01/src/controllers/register-vehicle.ts
@@ -1,12 +1,12 @@
 export class RegisterVehicle {
   handle (httpRequest: any): any {
-    if (httpRequest.body.name === undefined) {
+    if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('error in the: name')
       }
     }
-    if (httpRequest.body.model === undefined) {
+    if (!httpRequest.body.model) {
       return {
         statusCode: 400,
         body: new Error('error in the: model')
