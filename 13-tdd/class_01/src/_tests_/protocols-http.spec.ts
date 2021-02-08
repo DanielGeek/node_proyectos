d6307ed5696ca44e539de7e775eb88111452b617
DanielGeek <daniel30081990@gmail.com>
2021-02-07 23:08:20 -0300

    refactor: 14. Parse Url toBe and toEqual protocols HTTP and Query's

---
+++ 13-tdd/class_01/src/_tests_/protocols-http.spec.ts
@@ -0,0 +1,39 @@
+import { UrlLogin } from '../protocols/protocols-http'
+
+describe('Protocols Http and Querys', () => {
+  test('Url login ', () => {
+    const parsedUrl = UrlLogin.parseUrl('http://localhost:3000/login')
+
+    expect(parsedUrl.href).toBe('http://localhost:3000/login')
+    expect(parsedUrl.port).toBe('3000')
+  })
+
+  test('Response Query Login ', () => {
+    const parsedUrl = UrlLogin.parseUrl('http://localhost:3000/login?user=user&password=password')
+    const expectAuth = {
+      user: 'user',
+      password: 'password'
+    }
+
+    expect(parsedUrl.query).toEqual(expectAuth)
+  })
+
+  test('Url user ', () => {
+    const parsedUrl = UrlLogin.parseUrl('http://localhost:3000/user')
+
+    expect(parsedUrl.href).toBe('http://localhost:3000/user')
+    expect(parsedUrl.port).toBe('3000')
+  })
+
+  test('Response Query User ', () => {
+    const parsedUrl = UrlLogin.parseUrl('http://localhost:3000/user?user=user&password=password&name=name&lastname=lastname')
+    const expectAuthUser = {
+      user: 'user',
+      password: 'password',
+      name: 'name',
+      lastname: 'lastname'
+    }
+
+    expect(parsedUrl.query).toEqual(expectAuthUser)
+  })
+})
