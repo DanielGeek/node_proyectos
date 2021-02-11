2d2707b9ec27d70378c7ee276a3fc147090d8eed
DanielGeek <daniel30081990@gmail.com>
2021-02-11 00:03:07 -0300

    21. Interface segregation principle

---
+++ 13-tdd/class_01/src/_tests_/team.spec.ts
@@ -0,0 +1,19 @@
+import { Team } from '../models/team-model'
+
+describe('Team class', () => {
+  it('Position', () => {
+    const team = new Team()
+    const position = {
+      name: 'KAKA',
+      tShirt: 10,
+      skills: ['Kick hard', 'Run fast', 'Jump high']
+    }
+    expect(team.position(position.name, position.tShirt, position.skills)).toEqual(['KAKA', 10, ['Kick hard', 'Run fast', 'Jump high']])
+  })
+
+  it('Soccer Play', () => {
+    const soccerPlay = new Team()
+
+    expect(soccerPlay.attacker('attacker')).toBe('attacker')
+  })
+})
