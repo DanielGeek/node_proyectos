26497c15ad80c0a60bf751259a403e1302aa0e28
daniel-barreto <daniel30081990@gmail.com>
2022-10-13 23:55:13 -0300

    Serving the API on the web

--- 15-platzi-graphql/index.js
+++ 15-platzi-graphql/index.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const gqlMiddleware = require('express-graphql');
+
+const app = express();
+const port = process.env.port || 3000;
 
 // defining schema
 const schema = buildSchema(`
   type Query {
     hello: String
-    greeting: String
   }
 `)
 
@@ -14,13 +18,20 @@ const schema = buildSchema(`
 const resolvers = {
   hello: () => {
     return 'Hello World'
-  },
-  greeting: () => {
-    return 'Hi everyone'
   }
 }
 
-// Execute query hello
-graphql(schema, '{ greeting }', resolvers).then(( data ) => {
-  console.log(data);
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
 })
+
+// Execute query hello
+// graphql(schema, '{ greeting }', resolvers).then(( data ) => {
+//   console.log(data);
+// })
