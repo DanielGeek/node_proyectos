f5ea3995cf0547be830401bd729b13293a12e3d1
daniel-barreto <daniel30081990@gmail.com>
2022-10-16 01:49:42 -0300

    Review - Creating the Student type

--- 15-platzi-graphql/lib/mutations.js
+++ 15-platzi-graphql/lib/mutations.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const connectDB = require('./db')
+const { ObjectId } = require('mongodb')
 
 module.exports = {
   createCourse: async (root, { input }) => {
@@ -20,5 +21,56 @@ module.exports = {
       console.log(error)
     }
     return newCourse
+  },
+  createStudent: async (root, { input }) => {
+    let db
+    let student
+    try {
+      db = await connectDB()
+      student = await db.collection('students').insertOne(input)
+      input._id = student.insertedId
+    } catch (error) {
+      console.log(error)
+    }
+    return input
+  },
+  editCourse: async (root, { _id, input }) => {
+    let db
+    let course
+
+    try {
+      db = await connectDB()
+      await db.collection('courses').updateOne(
+        { _id: ObjectId(_id) },
+        { $set: input }
+      )
+      course = await db.collection('courses').findOne(
+        { _id: ObjectId(_id) }
+      )
+    } catch (error) {
+      console.error(error)
+    }
+
+    return course
+  },
+  editStudent: async (root, { _id, input }) => {
+    let db
+    let student
+
+    try {
+      db = await connectDB()
+      await db.collection('students').updateOne(
+        { _id: ObjectId(_id) },
+        { $set: input }
+      )
+      student = await db.collection('students').findOne(
+        { _id: ObjectId(_id) }
+      )
+    } catch (error) {
+      console.error(error)
+    }
+
+    return student
   }
+
 }
--- 15-platzi-graphql/lib/queries.js
+++ 15-platzi-graphql/lib/queries.js
@@ -27,5 +27,30 @@ module.exports = {
     }
 
     return course
+  },
+  getStudents: async () => {
+    let db
+    let students = []
+
+    try {
+      db = await connectDb()
+      students = await db.collection('students').find().toArray()
+    } catch (error) {
+      console.error(error)
+    }
+
+    return students
+  },
+  getStudent: async (root, { id }) => {
+    let db
+    let student
+    try {
+      db = await connectDb()
+      student = await db.collection('students').findOne({ _id: ObjectId(id) })
+    } catch (error) {
+      console.log(error)
+    }
+
+    return student
   }
 }
