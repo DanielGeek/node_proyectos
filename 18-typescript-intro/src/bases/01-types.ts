43d7b3bc71f3071297fd74d84de86ed6204ffd48
daniel-barreto <daniel30081990@gmail.com>
2022-11-04 00:20:17 -0300

    11. Data types - continuation

--- 18-typescript-intro/src/bases/01-types.ts
+++ 18-typescript-intro/src/bases/01-types.ts
@@ -1,5 +1,18 @@
-export const name = 'Daniel';
+export let name: string = 'Daniel';
 export const age: number = 32;
 export const isValid: boolean = true;
 
-console.log({ isValid });
+name = 'Jessica';
+
+export const templateString = ` This is a string
+multiline
+that can have
+" doubles
+' simple
+inyect values ${ name }
+expressions ${ 1 + 1 }
+numbers: ${ age }
+booleans: ${ isValid }
+`
+
+console.log( templateString );
