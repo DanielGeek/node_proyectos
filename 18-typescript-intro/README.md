2455431539b33657840cb8e0dcef95ba6959d834
daniel-barreto <daniel30081990@gmail.com>
2022-11-04 00:04:49 -0300

    10. Types and bases about modules

--- 18-typescript-intro/README.md
+++ 18-typescript-intro/README.md
@@ -4,3 +4,8 @@
 ```
 yarn create vite
 ```
+
+# Start proyect
+```
+yarn dev
+```
---
+++ 18-typescript-intro/src/bases/01-types.ts
@@ -0,0 +1,5 @@
+export const name = 'Daniel';
+export const age: number = 32;
+export const isValid: boolean = true;
+
+console.log({ isValid });
