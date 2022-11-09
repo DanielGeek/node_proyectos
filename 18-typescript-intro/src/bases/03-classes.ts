408df4f00587924312da4171acaef1a239ff0e11
daniel-barreto <daniel30081990@gmail.com>
2022-11-08 21:03:22 -0300

    15. Getters, methods and THIS

--- 18-typescript-intro/src/bases/03-classes.ts
+++ 18-typescript-intro/src/bases/03-classes.ts
@@ -3,7 +3,24 @@ export class Pokemon {
   // public id: number;
   // public name: string;
 
-  constructor( public readonly id: number, public name: string ) {}
+  get imageUrl(): string {
+    return `https://pokemon.com/${ this.id }.jpg`;
+  }
+
+  constructor(
+    public readonly id: number,
+    public name: string,
+    // public imageUrl: string
+  ) {}
+
+  scream() {
+    console.log(`${ this.name.toUpperCase() }!!!`);
+    this.speak();
+  }
+
+  speak() {
+    console.log(`${ this.name }, ${ this.name }`);
+  }
 
   // constructor( id: number, name: string ) {
   //   this.id = id;
@@ -16,3 +33,7 @@ export const charmander = new Pokemon( 4, 'Charmander' );
 
 // charmander.id = 10;
 // charmander.name = 'Mew';
+console.log(charmander.imageUrl);
+
+charmander.scream();
+// charmander.speak();
