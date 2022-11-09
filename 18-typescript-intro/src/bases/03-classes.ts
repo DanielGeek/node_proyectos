f0e25c2af2998d765613ae2e429d90671b52eaf4
daniel-barreto <daniel30081990@gmail.com>
2022-11-08 22:10:51 -0300

    17. Set data type to http responses (generic)

--- 18-typescript-intro/src/bases/03-classes.ts
+++ 18-typescript-intro/src/bases/03-classes.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Move, PokeapiResponse } from '../interfaces/pokeapi-response.interface';
 
 export class Pokemon {
 
@@ -24,9 +25,9 @@ export class Pokemon {
     console.log(`${ this.name }, ${ this.name }`);
   }
 
-  async getMoves() {
+  async getMoves(): Promise<Move[]> {
     // const moves = 10;
-    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+    const { data } = await axios.get<PokeapiResponse>('https://pokeapi.co/api/v2/pokemon/4');
     console.log( data.moves );
 
     return data.moves;
