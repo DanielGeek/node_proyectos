a86348a99f0af110150fea6b67dc26b79ea736f9
daniel-barreto <daniel30081990@gmail.com>
2022-11-12 21:36:28 -0300

    22. Method Decorator - @Deprecated

--- 18-typescript-intro/src/main.ts
+++ 18-typescript-intro/src/main.ts
@@ -2,8 +2,9 @@
 // import { name, age } from './bases/01-types';
 // import { charmander } from './bases/03-classes';
 // import { charmander } from './bases/04-injection';
+// import { charmander } from './bases/05-decorators';
 
-import { charmander } from './bases/05-decorators'
+import { charmander } from './bases/06-decorators2';
 
 import './style.css'
 
