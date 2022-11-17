cc38be843744eadf6df8cfc079b321aaef62c2d7
daniel-barreto <daniel30081990@gmail.com>
2022-11-17 20:56:03 -0300

    34. Get a car by ID

--- 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
+++ 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById( @Param('id') id: string ) {
+    console.log({ id });
+    return this.cars[id];
   }
 }
