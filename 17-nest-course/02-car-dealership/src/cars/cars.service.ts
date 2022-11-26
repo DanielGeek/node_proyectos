8d52b4bb8355e5fef7b0759512fba650ec4e0362
DanielGeek <daniel30081990@gmail.com>
2022-11-26 19:25:59 -0300

    38.Exception Filters

--- 17-nest-course/02-car-dealership/src/cars/cars.service.ts
+++ 17-nest-course/02-car-dealership/src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -26,7 +26,9 @@ export class CarsService {
   }
 
   findOneById( id: number ) {
+
     const car = this.cars.find( car => car.id === id );
+    if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
 
     return car;
   }
