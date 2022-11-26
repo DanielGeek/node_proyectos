c0403e80976cbe4c279fe9bf647cfa47317b9c95
DanielGeek <daniel30081990@gmail.com>
2022-11-26 19:43:28 -0300

    39. Post, Patch and Delete

--- 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
+++ 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -19,4 +19,25 @@ export class CarsController {
 
     return this.carsService.findOneById( id );
   }
+
+  @Post()
+  createCar( @Body() body: any ) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseIntPipe ) id: number,
+    @Body() body: any ) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar( @Param('id', ParseIntPipe ) id: number ) {
+    return {
+      method: 'delete',
+      id
+    };
+  }
+
 }
