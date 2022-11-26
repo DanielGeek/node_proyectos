38ea304e0684096bf33e5078752f614114286522
DanielGeek <daniel30081990@gmail.com>
2022-11-26 19:15:08 -0300

    Pipes

--- 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
+++ 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,8 +14,9 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById( @Param('id') id: string ) {
+  getCarById( @Param('id', ParseIntPipe ) id: number ) {
     console.log({ id });
-    return this.carsService.findOneById( +id );
+
+    return this.carsService.findOneById( id );
   }
 }
