f5432686489c4ea251c86c02802e67dbf26c586e
DanielGeek <daniel30081990@gmail.com>
2022-11-26 22:23:22 -0300

    46. DTO - Data Transfer Object

--- 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
+++ 17-nest-course/02-car-dealership/src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, ParseUUIDPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -20,8 +21,8 @@ export class CarsController {
   }
 
   @Post()
-  createCar( @Body() body: any ) {
-    return body;
+  createCar( @Body() createCarDto: CreateCarDto ) {
+    return createCarDto;
   }
 
   @Patch(':id')
---
+++ 17-nest-course/02-car-dealership/src/cars/dto/create-car.dto.ts
@@ -0,0 +1,8 @@
+
+
+export class CreateCarDto {
+
+  readonly brand: string;
+  readonly model: string;
+
+}
