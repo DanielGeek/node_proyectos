a1c3dede550912042155b928e18a332bc392d1bd
Daniel <daniel30081990@gmail.com>
2023-02-11 19:32:09 -0300

    12. Creating Restaurant Schema

--- 17-nest-course/03-restaurants_api/src/restaurants/restaurants.module.ts
+++ 17-nest-course/03-restaurants_api/src/restaurants/restaurants.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 
+import { RestaurantSchema } from './schemas/restaurant.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Restaurant', schema: RestaurantSchema },
+    ]),
+  ],
   controllers: [RestaurantsController],
   providers: [RestaurantsService],
 })
