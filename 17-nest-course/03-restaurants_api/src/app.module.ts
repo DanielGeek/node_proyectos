2b71d655ecbd5a2f506d025c5df86d09805546f5
Daniel <daniel30081990@gmail.com>
2023-02-11 19:16:48 -0300

    11. Connecting to MongoDB with mongoose

--- 17-nest-course/03-restaurants_api/src/app.module.ts
+++ 17-nest-course/03-restaurants_api/src/app.module.ts
@@ -3,8 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [RestaurantsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI_LOCAL),
+    RestaurantsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
