36d14e0ff0a43feb3855ec7a952725838618ec90
Daniel <daniel30081990@gmail.com>
2023-02-12 22:22:48 -0300

    13. Get all Restaurants and 14. Create a new Restaurant

--- 17-nest-course/03-restaurants_api/src/restaurants/restaurants.controller.ts
+++ 17-nest-course/03-restaurants_api/src/restaurants/restaurants.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './schemas/restaurant.schema';
 
 @Controller('restaurants')
-export class RestaurantsController {}
+export class RestaurantsController {
+  constructor(private restaurantsService: RestaurantsService) {}
+
+  @Get()
+  async getAllRestaurants(): Promise<Restaurant[]> {
+    return this.restaurantsService.findAll();
+  }
+
+  @Post()
+  async createRestaurant(@Body() restaurant): Promise<Restaurant> {
+    return this.restaurantsService.create(restaurant);
+  }
+}
--- 17-nest-course/03-restaurants_api/src/restaurants/restaurants.service.ts
+++ 17-nest-course/03-restaurants_api/src/restaurants/restaurants.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Restaurant } from './schemas/restaurant.schema';
 
 @Injectable()
-export class RestaurantsService {}
+export class RestaurantsService {
+  constructor(
+    @InjectModel(Restaurant.name)
+    private restaurantModel: mongoose.Model<Restaurant>,
+  ) {}
+
+  // Get all Restaurants => GET /restaurants
+  async findAll(): Promise<Restaurant[]> {
+    const restaurants = await this.restaurantModel.find();
+    return restaurants;
+  }
+
+  // Create new Restaurant => POST /restaurants
+  async create(restaurant: Restaurant): Promise<Restaurant> {
+    const restaurantCreated = await this.restaurantModel.create(restaurant);
+    return restaurantCreated;
+  }
+}
